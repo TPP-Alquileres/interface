39eebd2c215a87a7df62b31366a072bc93c3f081
Luciano Mintrone <luciano@amalgama.co>
2024-02-26 22:56:12 -0300

    Add SignIn button

---
+++ app/signin/page.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useRouter } from 'next/navigation';
+
+const SigninSchema = Yup.object().shape({
+  email: Yup.string().email('Correo electrónico inválido').required('Requerido'),
+  password: Yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('Requerido'),
+});
+
+export default function SignIn() {
+  const router = useRouter();
+
+  const submitForm = async ( formData ) => {
+    delete formData.confirmPassword;
+
+    const response = await fetch('/api/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.status === 200) { router.push('/'); }
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <Formik
+          initialValues={ { email: '', password: '', confirmPassword: '' } }
+          validationSchema={SigninSchema}
+          onSubmit={submitForm}
+        >
+          {props => (
+            <form onSubmit={props.handleSubmit}>
+              <>
+                <div className="space-y-6">
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">Correo electrónico</label>
+                    <Field
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                    <ErrorMessage name="email" component="div" />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">Contraseña</label>
+                    <Field
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                    <ErrorMessage name="password" component="div" />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center my-4">
+                    <input id="terms-and-conditions" name="terms-and-conditions" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+                    <label htmlFor="terms-and-conditions" className="ml-2 block text-sm text-gray-900"> Acepto los términos y condiciones </label>
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Log In
+                </button>
+              </>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  )
+}
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,13 +13,11 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Link
-              key="signup"
-              href="signup"
-            >
-              <div>
-                Sign Up
-              </div>
+            <Link key="signin" href="signin">
+              <div className="mr-3 decoration-solid font-medium hover:opacity-75">Sign In</div>
+            </Link>
+            <Link key="signup" href="signup">
+              <div className="rounded border border-current px-2 py-1 decoration-solid font-medium hover:opacity-75">Sign Up</div>
             </Link>
             <ThemeToggle />
           </nav>
