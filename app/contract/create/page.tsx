66e2fde9ab23dd385b75137bf1cb90303a582606
Santi Lazzari <santilazzari@Santis-MacBook-Pro.local>
2024-03-10 15:29:37 -0300

    Working view for contract

---
+++ app/contract/create/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/s"
+import { Button } from "@/components/ui/button"
+import { Contract } from "@/components/contract"
+
+export default function Component() {
+  return (
+    <Contract />
+  )
+}
