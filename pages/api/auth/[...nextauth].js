281197fb0d00a1d82e743fbc248d2bde03e2d2b8
Luciano Mintrone <luciano@amalgama.co>
2024-03-24 17:59:19 -0300

    Save User in DB after log in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
   providers: [
@@ -8,6 +11,27 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken
+      return session 
+    },
+    async signIn( { user } ) {
+      const aUser = await prisma.user.findUnique( { where: { email: user.email } } );
+      if (!aUser) {
+        await prisma.user.create( { data: 
+          { email: user.email, name: user.name } 
+        } );
+      }
+      return true
+    }
+  }
 }
 
 export default NextAuth(authOptions)
