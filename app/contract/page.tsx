c0abf7c5e085ccb96c15d33064232e5316998c46
Santi Lazzari <santilazzari@Santis-MacBook-Pro.local>
2024-04-14 17:38:42 -0300

    Contract model

---
+++ app/contract/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/s"
+import { Button } from "@/components/ui/button"
+import { ContractCreate } from "@/components/contract-create"
+
+export default function Component() {
+  return (
+    <ContractCreate />
+  )
+}
