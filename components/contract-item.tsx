bb747ced146d194060ca7802d1edb8502531343e
Pablo Israel <pablo@Pablos-MacBook-Pro.local>
2024-05-05 19:40:39 -0300

    Redirect to contracts view

--- components/contract-item.tsx
+++ components/contract-item.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import { Button } from "./ui/button";
+import { useRouter } from 'next/navigation';
+
 
 export default function ContractItem({contract, index}: any) {
     const tableRowClassName = index % 2 === 0 ? "bg-gray-100/40 dark:bg-gray-800/40" : "";
     const contractUrl = '/contracts/' + contract.id
+
+    const router = useRouter();
+
+    console.log(contractUrl);
     
     return (
         
-    <tr className={tableRowClassName}>
+    <tr key={contract.id} className={tableRowClassName}>
         <td className="px-6 py-3">{contract.description}</td>
         <td className="px-6 py-3">{contract.owner.name}</td>
         <td className="px-6 py-3">{contract.tenant?.name || "-"}</td>
         <td className="px-6 py-3">${contract.amount}</td>
         <td className="px-6 py-3">{contract.status}</td>
         <td className="px-6 py-3">
-          <Button size="sm" variant="outline">
+      <Button size="sm" variant="outline" onClick={() => router.push(contractUrl)}>
             Ver
           </Button>
           <Button size="sm" variant="outline">
